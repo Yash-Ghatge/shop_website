7baf0d59d681bc011b0f562bb2565cad137066f5
Yash-Ghatge <yashghatge012@gmail.com>
2025-05-15 20:01:51 +0530

    E-commerce Website

--- client/src/context/appContext.jsx
+++ client/src/context/appContext.jsx
@@ -5,7 +5,7 @@ const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
-  const [user, setUser] = useState(true);
+  const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
 
