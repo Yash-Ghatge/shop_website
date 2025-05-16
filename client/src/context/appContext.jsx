06646ad9a62c928afeede75181e2d01317ad829f
Yash-Ghatge <yashghatge012@gmail.com>
2025-05-16 20:49:38 +0530

    E-commerce Website

--- client/src/context/appContext.jsx
+++ client/src/context/appContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
-
+import toast, {Toaster} from "react-hot-toast";
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
@@ -13,9 +13,45 @@ const AppContextProvider = ({ children }) => {
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState({});
 
+  //fetch all products
   const fetchProducts = async ()=>{
     setProducts(dummyProducts)
+  }
+
+  // add product to cart
+  const addToCart =(itemId)=>{
+    let cartData =structuredClone(cartItems);
+    
+    if(cartData[itemId]){
+      cartData[itemId] += 1;
+    }else{
+      cartData[itemId] = 1;
+    }
+    setCartItems(cartData);
+    toast.success("Added to cart")
+  }
+
+  // update cart Item Quantity
+  const updateCartItem =(itemId,quantity)=>{
+    let cartData =structuredClone(cartItems);
+    cartData[itemId]=quantity;
+    setCartItems(cartData)
+    toast.success("Cart Updated")
+  }
+
+  // Remove Product from cart
+  const removeFromcart = (itemId) => {
+    let cartData =structuredClone(cartItems);
+    if(cartData[itemId]){
+      cartData[itemId] -= 1;
+      if(cartData[itemId]===0){
+        delete cartData[itemId];
+      }
+    }
+    toast.success("Removed from cart")
+    setCartItems(cartData)
   }
 
   useEffect(()=>{
@@ -31,7 +67,11 @@ const AppContextProvider = ({ children }) => {
     showUserLogin,
     setShowUserLogin,
     products,
-    currency
+    currency,
+    addToCart,
+    updateCartItem,
+    removeFromcart,
+    cartItems
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
