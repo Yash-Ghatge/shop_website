0dc08d0744c7997a3825bfd6138cad0c1841b244
Yash-Ghatge <yashghatge012@gmail.com>
2025-05-17 16:47:29 +0530

    E-Commerce Website

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           </button>
         </div>
 
-      {!user ? (<button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+      {!user ? (<button onClick={()=> setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
         Login
       </button>):(
         <div className="relative group">
--- client/src/context/appContext.jsx
+++ client/src/context/appContext.jsx
@@ -14,6 +14,7 @@ const AppContextProvider = ({ children }) => {
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
 
   //fetch all products
   const fetchProducts = async ()=>{
@@ -71,7 +72,9 @@ const AppContextProvider = ({ children }) => {
     addToCart,
     updateCartItem,
     removeFromcart,
-    cartItems
+    cartItems,
+    searchQuery,
+    setSearchQuery
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
