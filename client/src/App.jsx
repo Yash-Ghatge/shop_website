1bba444c669590d122b49ae96e971a33fbe1aa6f
Yash-Ghatge <yashghatge012@gmail.com>
2025-05-20 10:59:10 +0530

    E-Commerce Website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,8 @@ import MyOrders from './pages/MyOrders';
 import SellerLogin from './components/seller/SellerLogin';
 import SellerLayout from './pages/seller/SellerLayout';
 import AddProduct from './pages/seller/AddProduct';
+import Orders from './pages/seller/Orders';
+import ProductList from './pages/seller/ProductList';
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -35,6 +37,8 @@ const App = () => {
           <Route path='/my-orders' element={<MyOrders/>}/>
           <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
           <Route index element={isSeller ? <AddProduct/> : null}/>
+          <Route path='product-list' element={<ProductList/>}/>
+          <Route path='orders' element={<Orders/>}/>
           </Route>
         </Routes>
       </div>
