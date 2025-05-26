8dd85278171e24ba7ee5f0f276277c63598f2f8d
Yash-Ghatge <yashghatge012@gmail.com>
2025-05-26 21:34:15 +0530

    E-Commerce Website

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,9 +1,11 @@
 import Order from "../models/order.js";
 import Product from "../models/Product.js";
+import stripe from 'stripe';
 
 export const placeOrderCOD = async (req,res) => {
     try {
-        const { userId, items, address } = req.body;
+        const  userId  = req.userId;
+        const { items, address } = req.body;
         if(!address || !items.length === 0 ){
             return res.json({success:false,message:"Invaild data"})
         }
@@ -29,9 +31,74 @@ export const placeOrderCOD = async (req,res) => {
     }
 }
 
+export const placeOrderStripe = async (req,res) => {
+    try {
+        const  userId  = req.userId;
+
+        const { origin } = req.headers
+
+        const { items, address } = req.body;
+        if(!address || !items.length === 0 ){
+            return res.json({success:false,message:"Invaild data"})
+        }
+
+        let productData = [];
+
+        let amount = await items.reduce(async (acc,item) => {
+            const product = await Product.findById(item.product);
+            productData.push({
+                name:product.name,
+                price:product.offerPrice,
+                quantity:item.quantity
+            })
+            return (await acc) + product.offerPrice*item.quantity;
+        },0)
+        amount+=Math.floor(amount*0.02)
+
+        const order = await Order.create({
+            userId,
+            items,
+            amount,
+            address,
+            paymentType:"Online",
+        });
+
+        const stripeInstance = new stripe(process.env.STRIP_SECRET_KEY)
+
+        const line_items = productData.map((item)=>{
+            return {
+                price_data:{
+                    currency:"usd",
+                    product_data:{
+                        name:item.name,
+                    },
+                    unit_amount:Math.floor(item.price + item.price * 0.02)*100
+                },
+                quantity:item.quantity,
+            }
+        })
+
+        const session = await stripeInstance.checkout.sessions.create({
+            line_items,
+            mode:"payment",
+            success_url:`${origin}/loader?next=my-orders`,
+            cancel_url:`${origin}/cart`,
+            metadata:{
+                orderId:order._id.toString(),
+                userId,
+            }
+        })
+
+        return res.json({success:true,url:session.url});
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message});
+    }
+}
+
 export const getUserOrders = async (req,res) => {
     try {
-        const {userId} = req.body
+        const  userId  = req.userId;
         const orders = await Order.find({userId,$or:[{paymentType:"COD"},{ispaid:true}]}).populate("items.product address").sort({createdAt:-1});
         res.json({success:true,orders});
     } catch (error) {
--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllOrders, getUserOrders, placeOrderCOD } from '../controllers/orderController.js'
+import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderStripe } from '../controllers/orderController.js'
 import authUser from '../middleware/authUser.js';
 import authSeller from '../middleware/authSeller.js';
 
@@ -8,5 +8,6 @@ const orderRoute = express.Router()
 orderRoute.post('/cod',authUser,placeOrderCOD)
 orderRoute.get('/user',authUser,getUserOrders)
 orderRoute.get('/seller',authSeller,getAllOrders)
+orderRoute.post('/stripe',authUser,placeOrderStripe)
 
 export default orderRoute;
