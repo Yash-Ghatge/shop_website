3936d944e12e1f362425154ba8c7c61b40f17002
Yash-Ghatge <yashghatge012@gmail.com>
2025-06-02 15:28:01 +0530

    E-Commerce Website

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,8 +24,11 @@ export const register = async(req,res)=>{
 
         res.cookie('token',token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV === 'production',
-            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            // secure:process.env.NODE_ENV === 'production',
+            // sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
+
             maxAge : 7*24*60*60*1000,
         })
 
@@ -59,8 +62,11 @@ export const login = async(req,res)=>{
 
         res.cookie('token',token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV === 'production',
-            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            // secure:process.env.NODE_ENV === 'production',
+            // sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
+
             maxAge : 7*24*60*60*1000,
         })
 
@@ -89,8 +95,11 @@ export const logout = async(req,res)=>{
     try {
         res.clearCookie('token',{
             httpOnly:true,
-            secure:process.env.NODE_ENV === 'production',
-            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            // secure:process.env.NODE_ENV === 'production',
+            // sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite: 'none',
+
         })
         return res.json({success:true,message:"Logged Out"})
     } catch (error) {
